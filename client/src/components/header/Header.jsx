e363ab3462a416b2a8d34cfeef24f9ea2f6edf7b
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-08-27 18:53:29 -0400

    task: convert header to component

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header (props) {
+    const { highScore, score } = props;
+    return (
+        <header>
+            <div className="header--container">
+                <p>High Score: {highScore}</p>
+                <p>Score: {score}</p>
+            </div>
+        </header>
+    )
+}
