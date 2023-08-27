ece5bd0d8aa8bb6832468147ebac16d7015d0cfd
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-08-27 18:32:12 -0400

    task: convert end screen to component

---
+++ client/src/components/end-screen/EndScreen.jsx
@@ -0,0 +1,18 @@
+import "./end-screen.css";
+import Button from "../button/Button";
+
+export default function EndScreen (props) {
+    const { resetGame, score } = props;
+    return (
+        <div className="end-screen--container">
+            <h1 className="citric">Game Over</h1>
+            <h2>Your final score was: </h2>
+            <h2>
+                <span className="number">{score}</span>
+            </h2>
+            <div className="button--container">
+                <Button handleClick={resetGame} buttonText={"Play Again"}/>
+            </div>
+        </div>
+    )
+}
